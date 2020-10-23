a70637ad369deb7904149fd9b935389d62b97d08
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-10-23 15:06:38 -0400

    Merge data, update tooltip.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,69 @@
 const VIS_HT = window.innerHeight * .7, VIS_WID = window.innerWidth * .45; //~
-const MAX_BOXES_PER_ROW = 20, BOX_HT = 20, MARGIN = 2.5;
-let currRow = 1;
+const MAX_BOXES_PER_ROW = 20, MARGIN = 2.5;
+let currRow = 1, BOX_HT; //~
 
 var dataset = d3.csv('flavors_of_cacao.csv').get((dataset) => {
-	// dataset.forEach(record => console.log('record'));
-	// console.log(dataset[0]['Company (Maker-if known)']);
-	// console.log(dataset[0]['Specific Bean Origin or Bar Name']);
-	// console.log(dataset[0]['REF']);
-	// console.log(dataset[0]['Review Date']);
-	// console.log(dataset[0]['Rating']);
-	// console.log(dataset[0]['Bean Type']);
-	// console.log(dataset[0]['Broad Bean Origin']);
-	// console.log(dataset[0]['Company Location']);
-	console.log(dataset[0]['Rating']);
+	console.log(dataset.length)
+
+	BOX_HT = VIS_HT / (dataset.length / MAX_BOXES_PER_ROW);
+
+	// data cleaning
 	let removePercent = str => { return parseFloat(str.slice(0, -1)); }
 
-	// get unique cocoa vals ~ https://stackoverflow.com/a/35092559/1446598
 	let uniqueCocoaValues = [... new Set(dataset.map(choc => removePercent(choc['Cocoa Percent'])))];
-	console.log(uniqueCocoaValues)
-	
+
+	// helper method to sort by cocoa
 	let sortChocByCocoa = (c1, c2) => { return removePercent(c2['Cocoa Percent']) - removePercent(c1['Cocoa Percent']) } 
 
-	console.log((dataset))
 	var cocoaColorScale = d3.scaleLinear().domain([Math.min(...uniqueCocoaValues), Math.max(...uniqueCocoaValues)])//~
 			.range(["#bc8f8f", "#48240a"]);
 
 	// let sortChocByCocoa = (a,b) => { return parseFloat(b.Rating) - parseFloat(a.Rating) } 
 	dataset.sort((a,b) => { return b.Rating - a.Rating });
+
+	// 5 stars
 	let fiveStar = dataset.filter(choc => {
 		return (choc.Rating == 5);
-	});
-	
-	fiveStar.sort(sortChocByCocoa);
+	}).sort(sortChocByCocoa);
+
+	// 4 stars
 	let fourStar = dataset.filter(choc => {
-		return (choc.Rating >= 4);
-	});
+		return (choc.Rating >= 4 && choc.Rating < 5);
+	}).sort(sortChocByCocoa);
 
-	fourStar.sort(sortChocByCocoa);
-	
-	console.log(cocoaColorScale)
+	console.log(fourStar.length)
 
-	let toolTip = d3.select('#vis').append('div')
-		.attr('class', 'tooltip')
+	// 3 stars
+	let threeStar = dataset.filter(choc => {
+		return (choc.Rating >= 3 && choc.Rating < 4);
+	}).sort(sortChocByCocoa);
+
+	// 2 stars
+	let twoStar = dataset.filter(choc => {
+		return (choc.Rating >= 2 && choc.Rating < 1);
+	}).sort(sortChocByCocoa);
+
+	// 1 stars
+	let oneStar = dataset.filter(choc => {
+		return (choc.Rating >= 1 && choc.Rating < 2);
+	}).sort(sortChocByCocoa);
+
+	let all_chocolates = fourStar.concat(threeStar.concat(twoStar.concat(oneStar))); //~
+	let sortedChcolates = dataset.sort((c1, c2) => { return c1.Rating - c2.Rating || removePercent(c2['Cocoa Percent']) - removePercent(c1['Cocoa Percent']) });
+	console.log(sortedChcolates)
+	console.log(sortedChcolates[1360])
+	console.log(sortedChcolates[1790])
+	console.log(all_chocolates)
+	console.log(all_chocolates[1360])
+
+	let mouseToolTip = d3.select('#vis').append('div')
+		.attr('class', 'mouseTooltip')
 		.style('opacity', 0)
+	
+	// let sideToolTip = d3.select('#vis')
+	// .append('div')
+	// 	.attr('class', 'tooltip')
+	// 	.style('opacity', 0)
 
 	// container
 	var svg = d3.select('#vis')
@@ -64,48 +85,44 @@ var dataset = d3.csv('flavors_of_cacao.csv').get((dataset) => {
 
 	let currWid = fourStar.length < MAX_BOXES_PER_ROW ? VIS_WID / fourStar.length : VIS_WID / MAX_BOXES_PER_ROW;
 	svg.selectAll('rect')
-		.data(fourStar)
+		.data(all_chocolates)
 		.enter()
 		.append('rect')
+
+		// debug
+		.attr('stroke', 'red')
+		.attr('stroke-width', '1')
+		
 		.attr('x', (d, i) => {	
 			return (i);// * currWid;//~
 			// return (i % MAX_BOXES_PER_ROW) * currWid;
 			// return ((i % MAX_BOXES_PER_ROW) * 50); 
-			// return (i * fourStar.length / MAX_BOXES_PER_ROW) + (MARGIN * i); 
+			// return (i * all_chocolates.length / MAX_BOXES_PER_ROW) + (MARGIN * i); 
 		})
 		.attr('y', (d, i) => { return ((i % MAX_BOXES_PER_ROW) * MARGIN * BOX_HT); })// 
-		.attr('width', currWid * 5)//_ => { return currWid; } //return (fourStar.length / MAX_BOXES_PER_ROW) * MARGIN; })
+		.attr('width', currWid * 5)//_ => { return currWid; } //return (all_chocolates.length / MAX_BOXES_PER_ROW) * MARGIN; })
 		.attr('height', BOX_HT * 2.2)
 		.attr('fill', (d) => { return cocoaColorScale(removePercent(d['Cocoa Percent'])); })
 		
 		// tooltip
 		.on('mouseover', d => {
-			toolTip.transition()
+			mouseToolTip.transition()
 				.duration(250)//~
 				.style('opacity', 1);
 
-			toolTip.html(`Cocoa:  + ${ d['Cocoa Percent'] }`)
+			mouseToolTip.html(`
+			Cocoa: ${ d['Cocoa Percent'] }
+			<br/>
+			Rating: ${ d['Rating'] }/5
+			`)
 				.style('left', `${ d3.event.pageX }px`)
 				.style('top', `${ d3.event.pageY }px`);
-
+				
+				
+			// sideToolTip.html(`Cocoa:  + ${ d['Cocoa Percent'] }`)
+			// 	.style('left', `${ d3.event.pageX }px`)
+			// 	.style('top', `${ d3.event.pageY }px`);
 		})
 
-
-		// svg.selectAll('rect')
-		// .data(fiveStar)
-		// .enter()
-		// .append('rect')
-		// .attr('x', (d, i) => { 		
-		// 	return (i % MAX_BOXES_PER_ROW) * currWid;
-		// 	// return ((i % MAX_BOXES_PER_ROW) * 50); 
-		// 	// return (i * fiveStar.length / MAX_BOXES_PER_ROW) + (MARGIN * i); 
-		// })
-		// .attr('y', (d, i)=> { return ((i % MAX_BOXES_PER_ROW) * MARGIN * BOX_HT); })// 
-		// .attr('width', _ => { return (fiveStar.length / MAX_BOXES_PER_ROW) * MARGIN; })
-		// .attr('height', BOX_HT * 2)
-		// .attr('fill', (d) => { return cocoaColorScale(removePercent(d['Cocoa Percent'])); 
-		// 	// d['Cocoa Percent'] * 0.5 
-		// })
-
-		console.log(fourStar.length / MAX_BOXES_PER_ROW)
+		console.log(all_chocolates.length / MAX_BOXES_PER_ROW)
 });
--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@ Assignment for class 5010. Chocolate ratings visualized, built with D3.js.
 
 ## Data Source
 - Data source: [Kaggle](https://www.kaggle.com/rtatman/chocolate-bar-ratings)
+
+## Handy refs
+- [get unique cocoa vals](https://stackoverflow.com/a/35092559/1446598)  
